d0da4e4f07bf205be101d5f1a5e8172c71c545db
Macr0s <matte0809@gmail.com>
2016-10-21 11:11:33 +0200

    struttura date

--- 2016-10-21/workshop_02.py
+++ 2016-10-21/workshop_02.py
@@ -1,4 +1,7 @@
 from pyplasm import *
+import csv
+
+"""http://www.calcolostrutture.com/public/uploaded/News_911_01.jpg"""
 
 def ggpl_bone_structure(file_name):
     def planeStructure(beamSection, pillarSection, distancePillars, intersectHeights):
@@ -27,15 +30,47 @@ def ggpl_bone_structure(file_name):
 
         startDistance = 0
         for iterator in range(len(distancePillars)):
-            if iterator != len(distancePillars) - 1:
-                fStructs.append(createFStruct(startDistance, height, distancePillars[iterator]))
-            else:
-                fStructs.append(createFinalStruct(startDistance, height))
+            fStructs.append(createFStruct(startDistance, height, distancePillars[iterator]))
 
             startDistance = startDistance + pillarSection[1] + distancePillars[iterator]
 
+        fStructs.append(createFinalStruct(startDistance, height))
+
         return STRUCT(fStructs)
 
+    def createBaseFrame(origin, beamSection, pillarSection, distancePillars, intersectHeights):
+        return STRUCT([
+            T([1,2,3])([origin[0], origin[1], origin[2]]),
+            planeStructure(beamSection, pillarSection, distancePillars, intersectHeights)
+        ])
+
+    def createTrasversalBean():
+        return 0
+
+    frames = []
+    odd = True
+    origin = [0, 0, 0]
+    with open(file_name, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            if odd :
+                odd = False
+                origin = [origin[0] + float(row[0]), origin[1] + float(row[1]), origin[2] + float(row[2])]
+            else:
+                odd = True
+                beamSection = [float(row[0]), float(row[1])]
+                pillarSection = [float(row[2]), float(row[3])]
+
+                distancePillars = []
+                for index in range(5, int(row[4]) + 5):
+                    distancePillars.append(int(row[index]))
+
+                intersectHeights = []
+                for index in range(int(row[4]) + 6, len(row)):
+                    intersectHeights.append(int(row[index]))
+
+                frames.append(createBaseFrame(origin, beamSection, pillarSection, distancePillars, intersectHeights))
+    return STRUCT(frames)
 
 
-VIEW(ggpl_bone_structure("frame_data_457024.csv"))
+VIEW(ggpl_bone_structure("simple_frame_data_457024.csv"))
