419aaaadf0d28e3d9b2dbfe33eb33b25948549c2
Macr0s <matte0809@gmail.com>
2016-10-22 10:56:24 +0200

    Astrazione parser CSV

--- 2016-10-21/workshop_02.py
+++ 2016-10-21/workshop_02.py
@@ -3,6 +3,7 @@ import csv
 
 """http://www.calcolostrutture.com/public/uploaded/News_911_01.jpg"""
 
+
 def ggpl_bone_structure(file_name):
     def planeStructure(beamSection, pillarSection, distancePillars, intersectHeights):
         fStructs = []
@@ -63,53 +64,48 @@ def ggpl_bone_structure(file_name):
                 QUOTE(z)
         ])
 
-    frames = []
-    odd = True
-    distance = 0
-    transaction = None
-    first = True
-    pillarSection = []
-    beamSection = []
-    distancePillars = []
-    intersectHeights = []
-    with open(file_name, 'rb') as csvfile:
-        builderreader = csv.reader(csvfile)
+    def parseCSV(file_name):
+        odd = True
+        transaction = None
+        with open(file_name, 'rb') as csvfile:
+            builderreader = csv.reader(csvfile)
 
-        for row in builderreader:
-            if odd :
-                odd = False
-                distance = float(row[0])
-                transaction = T([1,2,3])([float(row[0]), float(row[1]), float(row[2])])
-
-                if not first and distance != 0:
-                    frames.append(
-                        createTrasversalBeam(distance, beamSection, pillarSection, distancePillars, intersectHeights)
-                    )
+            for row in builderreader:
+                if odd:
+                    odd = False
+                    transaction = [float(row[0]), float(row[1]), float(row[2])]
                 else:
-                    first = False
-            else:
-                odd = True
-                beamSection = [float(row[0]), float(row[1])]
-                pillarSection = [float(row[2]), float(row[3])]
+                    odd = True
+                    beamSection = [float(row[0]), float(row[1])]
+                    pillarSection = [float(row[2]), float(row[3])]
 
-                distancePillars = []
-                start_point =  5
-                finish_point = int(row[4]) + start_point
-                for index in range(start_point, finish_point):
-                    distancePillars.append(int(row[index]))
+                    distancePillars = []
+                    start_point = 5
+                    finish_point = int(row[4]) + start_point
+                    for index in range(start_point, finish_point):
+                        distancePillars.append(int(row[index]))
 
-                intersectHeights = []
-                start_point = finish_point + 1
-                finish_point = int(row[finish_point]) + start_point
-                for index in range(start_point, finish_point):
-                    intersectHeights.append(int(row[index]))
+                    intersectHeights = []
+                    start_point = finish_point + 1
+                    finish_point = int(row[finish_point]) + start_point
+                    for index in range(start_point, finish_point):
+                        intersectHeights.append(int(row[index]))
 
-                frames.append(transaction)
-                frames.append(
-                    planeStructure(beamSection, pillarSection, distancePillars, intersectHeights)
-                )
+                    yield transaction, beamSection, pillarSection, distancePillars, intersectHeights
 
+    frames = []
+    distance = 0
+    for transaction, beamSection, pillarSection, distancePillars, intersectHeights in parseCSV(file_name):
+        if transaction[0] != 0:
+            distance = transaction[0]
 
+        frames.append(
+            createTrasversalBeam(distance, beamSection, pillarSection, distancePillars, intersectHeights)
+        )
+        frames.append(T([1, 2, 3])(transaction))
+        frames.append(
+            planeStructure(beamSection, pillarSection, distancePillars, intersectHeights)
+        )
 
     return STRUCT(frames)
 
