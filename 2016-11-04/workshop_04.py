21738a229c32d9c02120af8ebe75ed739e73d7f1
Macr0s <matte0809@gmail.com>
2016-11-05 14:44:49 +0100

    Aggiunta la prima versione del notebook

--- 2016-11-04/workshop_04.py
+++ 2016-11-04/workshop_04.py
@@ -1,5 +1,18 @@
+
+# coding: utf-8
+
+# # Workshop 4 - Hip Roof
+# 
+# ![hip_roof](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/Rectangular-hip-roof.png)
+
+# In[ ]:
+
 from pyplasm import *
 
+
+# ## Versione 1
+
+# In[26]:
 
 def ggpl_hip_roof(verts, cells):
     roof = []
@@ -40,7 +53,55 @@ def ggpl_hip_roof(verts, cells):
     roof.append(struct)
     return STRUCT(roof)
 
-def ggpl_hip_roof_hpc(roof):
+
+# ### Esempio 1
+
+# In[27]:
+
+VIEW(ggpl_hip_roof([
+    [0,0,0],[0,10,0],[5,5,0],[5,5,5],[15,0,0],[15,10,0],[10,5,0],[10,5,5]
+],
+[
+    [3,4,2,1],[8,5,6,7],[5,8,7,3,4,1],[2,4,3,7,8,6]
+]))
+
+
+# #### Risultato
+# 
+# ![example1.1](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/example1.1.png)
+# 
+# ![example1.2](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/example1.2.png)
+
+# ### Esempio 2
+
+# In[28]:
+
+VIEW(ggpl_hip_roof([
+        [0, 10, 0], [4, 10, 0], [2, 8, 0], [2, 2, 0], [2, 8, 4], [2, 2, 4],
+        [4, 4, 0], [0, 0, 0], [10, 0, 0], [10, 4, 0], [8, 2, 0], [8, 2, 4]
+],[
+        [1, 2, 3, 5],
+        [1, 8, 4, 3, 5, 6], 
+        [2, 7, 6, 5, 3, 4],
+        [8, 9, 11, 4, 6, 12],
+        [9, 10, 11, 12],
+        [7, 10, 11, 4, 6, 12]
+]))
+
+
+# #### Risultato
+# 
+# ![example2.1](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/example2.1.png)
+# 
+# ![example2.2](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/example2.2.png)
+# 
+# ![example2.3](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/example2.3.png)
+
+# ## Versione 2
+
+# In[14]:
+
+def ggpl_hip_roof_hpc(struct):
     def cleanUKPOL(skel):
         points, cells, none = UKPOL(skel)
 
@@ -85,11 +146,7 @@ def ggpl_hip_roof_hpc(roof):
         cells_new = map(searchFN, cells)
 
         return [points, cells_new]
-
-    verts, cells = cleanUKPOL(roof)
-
-    roof = []
-
+    
     def makeRidge(verts, cells):
         remove = []
 
@@ -116,38 +173,23 @@ def ggpl_hip_roof_hpc(roof):
             ridge
         ])
 
-    def makeStructure(verts, cells):
-        return OFFSET([0.1, 0.2, 0.1])(SKEL_1(MKPOL([verts, cells, None])))
+    verts, cells = cleanUKPOL(struct)
+    print verts
+    print cells
+
+    roof = []
 
     ridge = makeRidge(verts, cells)
-    struct = makeStructure(verts, cells)
+    struct = OFFSET([0.1, 0.2, 0.1])(SKEL_1(struct))
 
     roof.append(COLOR(RED)(ridge))
     roof.append(struct)
     return STRUCT(roof)
 
-VIEW(ggpl_hip_roof([
-    [0,0,0],[0,10,0],[5,5,0],[5,5,5],[15,0,0],[15,10,0],[10,5,0],[10,5,5]
-],
-[
-    [3,4,2,1],[8,5,6,7],[5,8,7,3,4,1],[2,4,3,7,8,6]
-]))
 
-VIEW(ggpl_hip_roof([
-    [0, 0, 0], [10, 0, 0], [8, 2, 4], [2, 2, 4], [2, 2, 0], [8, 2, 0],
-    [8, 2, 0], [2, 2, 0], [2, 2, 4], [8, 2, 4], [10, 4, 0], [4, 4, 0],
-    [10, 0, 0], [10, 4, 0], [8, 2, 0], [8, 2, 4],
-    [0, 0, 0], [0, 10, 0], [2, 8, 0], [2, 2, 0], [2, 2, 4], [2, 8, 4],
-    [2, 2, 0], [4, 4, 0], [4, 10, 0], [2, 8, 0], [2, 2, 4], [2, 8, 4],
-    [0, 10, 0], [4, 10, 0], [2, 8, 0], [2, 8, 4]
-],[
-    [1, 2, 3, 4, 5, 6],
-    [7, 8, 9, 10, 11, 12],
-    [13, 14, 15, 16],
-    [17, 18, 19, 20, 21, 22],
-    [23, 24, 25, 26, 27, 28],
-    [29, 30, 31, 32]
-]))
+# ### Esempio 1
+
+# In[29]:
 
 VIEW(ggpl_hip_roof_hpc(MKPOL([
     [
@@ -158,3 +200,17 @@ VIEW(ggpl_hip_roof_hpc(MKPOL([
     ],
     None
 ])))
+
+
+# #### Risultato
+# 
+# ![example3.1](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/example3.1.png)
+# 
+# ![example3.2](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/example3.2.png)
+# 
+# ![example3.3](https://raw.githubusercontent.com/Macr0s/ggpl/master/2016-11-04/images/example3.3.png)
+
+# In[ ]:
+
+
+
