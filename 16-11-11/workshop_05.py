ea086be0b8c6d8af4527ab661600068d7e7ea2f3
Macr0s <matte0809@gmail.com>
2016-11-11 11:02:18 +0100

    Aggiunta creazione sedia con braccioli

--- 16-11-11/workshop_05.py
+++ 16-11-11/workshop_05.py
@@ -1,6 +1,13 @@
 from pyplasm import *
 
 
+def intRGBColor(values):
+    return Color4f([values[0] / 255.0,
+                    values[1] / 255.0,
+                    values[2] / 255.0,
+                    1.0])
+
+
 def ggpl_chair(dx, dy, dz, depth_leg=0.015, distance_leg=0.03, depth_chair=0.01, height_leg=0.45):
     def makeLeg(dx, dy, dz):
         def baseChair(dx, dy, dz):
@@ -35,12 +42,6 @@ def ggpl_chair(dx, dy, dz, depth_leg=0.015, distance_leg=0.03, depth_chair=0.01,
             baseChair(dx, dy, dz)
         ])
 
-    def intRGBColor(values):
-        return Color4f([values[0] / 255.0,
-                        values[1] / 255.0,
-                        values[2] / 255.0,
-                        1.0])
-
     return STRUCT([
         COLOR(BLACK)(makeLeg(dx, dy, height_leg)),
         T(3)(height_leg),
@@ -49,14 +50,60 @@ def ggpl_chair(dx, dy, dz, depth_leg=0.015, distance_leg=0.03, depth_chair=0.01,
         COLOR(intRGBColor([215, 190, 157]))(CUBOID([dx, depth_chair, dz - depth_chair - height_leg]))
     ])
 
-
 def ggpl_chair_with_arm(dx, dy, dz, depth_leg=0.015, distance_leg=0.03, depth_chair=0.01, height_leg=0.45):
-    chair = ggpl_chair(dx, dy, dz, depth_leg, distance_leg, depth_chair, height_leg)
+    chair = ggpl_chair(dx - depth_leg * 4, dy, dz, depth_leg, distance_leg, depth_chair, height_leg)
 
+    def makeArm(dx, dy, dz, right=True):
+        def supportArm():
+            return COLOR(BLACK)(STRUCT([
+                T([2,3])([distance_leg,height_leg - depth_leg]),
+                CUBOID([depth_leg * 2 + distance_leg, depth_leg, depth_leg]),
+                T([2])([dy - distance_leg * 2 - depth_leg]),
+                CUBOID([depth_leg * 2 + distance_leg, depth_leg, depth_leg])
+            ]))
 
+        base = [
+            COLOR(BLACK)(PROD([
+                PROD([
+                    QUOTE([0, dx]),
+                    QUOTE([-distance_leg, depth_leg, - (dy - distance_leg * 2 - depth_leg * 2), depth_leg])
+                ]),
+                QUOTE([-height_leg, (dz - height_leg - depth_leg) * 0.5])
+            ])),
+            COLOR(intRGBColor([215, 190, 157]))(PROD([
+                PROD([
+                    QUOTE([0, dx]),
+                    QUOTE([-distance_leg, dy - distance_leg * 2])
+                ]),
+                QUOTE([- (height_leg + (dz - height_leg - depth_leg) * 0.5), depth_leg])
+            ]))
+        ]
 
-def ggpl_desk_with_chair(dx, dy, dz):
+        final = [supportArm()]
 
-    return ggpl_chair_with_arm(dx, dy, dz)
+        if not right:
+            final = [
+                supportArm(),
+                T(1)(distance_leg + depth_leg)
+            ]
 
-VIEW(ggpl_desk_with_chair(0.40, 0.40, 0.9))
+        final.extend(base)
+
+        return STRUCT(final)
+
+    return STRUCT([
+        makeArm(depth_leg, dy, dz),
+        T(1)(depth_leg*2),
+        chair,
+        T(1)(dx - depth_leg * 4 - distance_leg),
+        makeArm(depth_leg, dy, dz, False)
+    ])
+
+def ggpl_chair_with_desk(dx, dy, dz, depth_leg=0.015, distance_leg=0.03, depth_chair=0.01, height_leg=0.45):
+    chair = ggpl_chair_with_arm(dx, dy, dz, depth_leg, distance_leg, depth_chair, height_leg)
+
+    return chair
+
+v = ggpl_chair_with_desk(0.40, 0.40, 0.9)
+print SIZE([1,2,3])(v)
+VIEW(v)
