c00cd1271ff79f492950bf31bf8b11aeb34e4a94
Macr0s <matte0809@gmail.com>
2016-12-02 11:41:20 +0100

    Generazione struttura base

--- 2016-12-02/workshop_8.py
+++ 2016-12-02/workshop_8.py
@@ -2,7 +2,7 @@ from pyplasm import *
 import csv
 
 
-def createStructFromLines(file_name, size, color):
+def createStructFromLines(file_name, size):
     points = []
     indexs = []
     i = 0
@@ -16,24 +16,45 @@ def createStructFromLines(file_name, size, color):
             i += 2
             indexs.append([i - 1, i])
 
-    return COLOR(color)(
-        OFFSET([size, size])(
-            MKPOL([
-                points,
-                indexs,
-                None
-            ])
-        )
+    return OFFSET([size, size])(
+        MKPOL([
+            points,
+            indexs,
+            None
+        ])
+    )
+
+def createFloorFromLines(file_name, size):
+    points = []
+    indexs = []
+    i = 0
+
+    with open(file_name, 'rb') as csvfile:
+        builderreader = csv.reader(csvfile)
+
+        for row in builderreader:
+            points.append([float(row[0]), float(row[1])])
+            points.append([float(row[2]), float(row[3])])
+            i += 2
+            indexs.extend([i - 1, i])
+
+    return OFFSET([size, size])(
+        MKPOL([
+            points,
+            [indexs],
+            None
+        ])
     )
 
 
 if __name__ == "__main__":
-    externalWall = createStructFromLines("pianimetria/lines/Muro Esterno.lines", 4, RED)
-    internalWall = createStructFromLines("pianimetria/lines/Strutture interne.lines", 4, GREEN)
-    windows = createStructFromLines("pianimetria/lines/Finestre.lines", 4, BLACK)
-    doors = createStructFromLines("pianimetria/lines/Porte.lines", 4, WHITE)
-    pillars = createStructFromLines("pianimetria/lines/Colonne Interne.lines", 4, MAGENTA)
-    balconies = createStructFromLines("pianimetria/lines/Terrazzi.lines", 4, ORANGE)
+    externalWall = createStructFromLines("pianimetria/lines/Muro Esterno.lines", 4)
+    internalWall = createStructFromLines("pianimetria/lines/Strutture interne.lines", 4)
+    windows = createStructFromLines("pianimetria/lines/Finestre.lines", 6)
+    doors = createStructFromLines("pianimetria/lines/Porte.lines", 6)
+    pillars = createStructFromLines("pianimetria/lines/Colonne Interne.lines", 4)
+    balconies = createStructFromLines("pianimetria/lines/Terrazzi.lines", 4)
+    internalFloor = createFloorFromLines("pianimetria/lines/Muro Esterno.lines", 4)
 
     external = DIFF([
         externalWall,
@@ -48,13 +69,16 @@ if __name__ == "__main__":
     ])
 
     VIEW(STRUCT([
-        PROD([
+        COLOR(RED)(PROD([
             external,
             QUOTE([100])
-        ]),
-        PROD([
+        ])),
+        COLOR(BLUE)(PROD([
             internal,
             QUOTE([100])
-        ]),
+        ])),
+        pillars,
+        balconies,
+        internalFloor
     ]))
 
