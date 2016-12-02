875f03f47b5e9b2dbab99c2f35fbab89f3c4ef4f
Macr0s <matte0809@gmail.com>
2016-12-02 11:02:16 +0100

    Aggiunti terrazzi

--- 2016-12-02/workshop_8.py
+++ 2016-12-02/workshop_8.py
@@ -1,2 +1,33 @@
 from pyplasm import *
+import csv
+
+
+def createStructFromLines(file_name):
+    points = []
+    indexs = []
+    i = 0
+
+    with open(file_name, 'rb') as csvfile:
+        builderreader = csv.reader(csvfile)
+
+        for row in builderreader:
+            points.append([float(row[0]), float(row[1])])
+            points.append([float(row[2]), float(row[3])])
+            i += 2
+            indexs.append([i - 1, i])
+
+    return MKPOL([
+        points,
+        indexs,
+        None
+    ])
+
+
+if __name__ == "__main__":
+    externalWall = createStructFromLines("pianimetria/lines/Muro Esterno.lines")
+    internalWall = createStructFromLines("pianimetria/lines/Strutture interne.lines")
+    windows = createStructFromLines("pianimetria/lines/Finestre.lines")
+    doors = createStructFromLines("pianimetria/lines/Porte.lines")
+    pillars = createStructFromLines("pianimetria/lines/Colonne Interne.lines")
+    balconies = createStructFromLines("pianimetria/lines/Terrazzi.lines")
 
